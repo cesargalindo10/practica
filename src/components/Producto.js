2a3de68ef15c0c232ebf60f58a863d4c7c7688d4
cesar <fcgvcesar@gmail.com>
2023-02-09 07:18:24 -0400

    paginacion render

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,28 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import { ApiServices } from "../services/api.services"
-import React, { useEffect, useState } from 'react';
+
 
 
 export default function Producto() {
 
 
-    const [producto, setProducto] = useState([])
+    const [data, setData] = useState([]);
+    const [paginacion, setPaginacion] = useState([]);
+    const [page, setPage]=useState();
 
     const loadProducto = async () => {
-        ApiServices.getProductos()
-            .then((response) => {
-            
-                setProducto(response)
-                console.log(response)
-            })
+        await ApiServices.getProductos(page)
+            .then(res => {
+                setData(res.data)
+                setPaginacion(res.pagination)
+            }
+            )
+
     }
 
     useEffect(() => {
         loadProducto()
-    }, [])
+    }, [page])
 
     return (
         <div>
-
+            {
+                data.map((item) =>
+                    <div key={item.id}>
+                        <h3 key={item.id}>{item.nombre}</h3>
+                    </div>
+                )
+            }
+            <button onClick={() => setPage(paginacion.paginaAnterior)}>Anterior</button>
+            <button onClick={() => setPage(paginacion.PaginaSiguiente)}>Siguiente</button>
         </div>
     );
 
--- src/services/api.services.js
+++ src/services/api.services.js
@@ -1,15 +1,30 @@
+
 const APIURL = process.env.REACT_APP_API_URL;
+
 export const ApiServices = {
-    getProductos: async()=>{
-        const resultado = await  fetch(APIURL+"producto/index")
-        .then((res)=>{
-            res.json();
-            console.log(res)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
-        return resultado;
+    getProductos : async (page) => {
+        //let response = {} ;
+        try {
+          const response = await fetch(APIURL+"producto/index?page="+page)
+          const data = await response.json();
+          return data;
+          
+        } catch (error) {
+            console.error("Se produjo un error", error);
+        }
+    },
+    createProducto:async(data)=>{
+        fetch(APIURL+'producto/update', {
+            method: 'POST', 
+            body: JSON.stringify(data), 
+            headers:{
+              'Content-Type': 'application/json'
+            }
+          }).then(res => res.json())
+          .catch(error => console.error('Error:', error))
+          .then(response => console.log('Success:', response));
     }
+    
+    
     
 }
